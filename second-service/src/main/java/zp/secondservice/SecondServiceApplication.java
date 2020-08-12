d6010d8a420abaa118978952109b3ebdf8082cfa
thangvynam <thangvynam1808@gmail.com>
2020-08-12 09:36:21 +0700

    format code

--- second-service/src/main/java/zp/secondservice/SecondServiceApplication.java
+++ second-service/src/main/java/zp/secondservice/SecondServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecondServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecondServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecondServiceApplication.class, args);
+    }
 
 }
